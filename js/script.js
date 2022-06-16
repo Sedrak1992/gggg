6ab91be82442357272c1161d2847814e799772d7
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2022-06-16 21:08:28 +0400

    a

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+// const app = document.querySelector(".app")
+// app.innerHTML = `
+// <div>
+//     <h1>
+//         Turn the light on and off
+//         <br/>
+//         by clicking the bulb!
+//     </h1>
+//     <div  class="divBox">
+//         <img src="img/OFFbulb.jpeg" alt="lamp" class=" iimg"/>
+//         <img src="img/ONbulb.jpeg" alt="lamp" class="imgLampMiacrac "/>
+//     </div>
+// </div>
+//   `;
+// const imgLalp = document.querySelector(".divBox");
+// // const imgLampMiacrac = document.querySelector(".imgLampMiacrac");
+
+// const f = () => {
+//   // imgLampMiacrac.classList.toggle("dexin");
+//   imgLalp.classList.toggle("dexin");
+// };
+
+// imgLalp.onclick = f;
+// // imgLampMiacrac.onclick = f;
